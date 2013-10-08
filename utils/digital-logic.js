7b5b82504e4be9c9c650e8017bdfe88e5bb34dcb
wrongu <lange.richard.d@gmail.com>
2013-10-08 14:02:00 -0400

    functioning without use of true or false literals
    everything done as objects, with as-much-as-possible evaluation where variables don't all have values

--- utils/digital-logic.js
+++ utils/digital-logic.js
@@ -14,6 +14,29 @@
  *
  */
 
+function isLiteral(obj){
+	return obj.TYPE === "LITERAL";
+}
+
+function isNonLitereal(obj){
+	return !isLiteral(obj);
+}
+
+function isOperator(obj){
+	return obj.TYPE === "OPERATOR";
+}
+
+function isVariable(obj){
+	return obj.TYPE == "VARIABLE";
+}
+
+function containsExpression(array, proto){
+	for(var i=0; i < array.length; i++){
+		if(array[i].NAME === proto.NAME) return true;
+	}
+	return false;
+}
+
 var ExpressionError = function(message){
 	this.message = message;
 };
@@ -25,21 +48,38 @@ ExpressionError.prototype.name = "ExpressionError";
 function AND(ops_array){
 	this.operands = ops_array;
 }
+AND.prototype.TYPE = "OPERATOR";
+AND.prototype.NAME = "AND";
 
 AND.prototype.str = function(){
-	return $.map(this.operands, function(elem, index){
-		return elem.str();
+	return $.map(this.operands, function(evaled, index){
+		return evaled.str();
 	}).join(" AND ");
 };
 
 AND.prototype.eval = function(){
-	for (var i = 0; i < this.operands.length; i++) {
-		if(!this.operands[i].eval()){
-			return false;
-		}
-	};
-	return true;
-};
+	var evaled = $.map(this.operands, function(evaled, ind){
+		return evaled.eval() || evaled;
+	});
+	var literals = $.grep(evaled, isLiteral);
+	var symbols = $.grep(evaled, isNonLitereal);
+	// Basically, evaluation converts to literal TRUE/FALSE values
+	//	whenever possible. Sometimes, due to variables being mixed
+	//	into the expressions, non-literals ("symbols") still exist
+	//	in the expression. If they do, we need to return the AND
+	//	of whatever symbols remain.
+	//	However! We also know that this is an AND expression. Any
+	//	FALSE in the literals will falsify everything, regardless
+	//	of what the symbols eventually turn out to be.
+	if(containsExpression(literals, FALSE))
+		return FALSE;
+	else if(symbols.length > 1)
+		return new AND(symbols);
+	else if(symbols.length == 1)
+		return symbols[0];
+	else
+		return TRUE;
+}
 
 /*********
  ** OR  **
@@ -49,19 +89,30 @@ function OR(ops_array){
 	this.operands = ops_array;
 }
 
+OR.prototype.TYPE = "OPERATOR";
+OR.prototype.NAME = "OR";
+
 OR.prototype.str = function(){
-	return "(" + $.map(this.operands, function(elem, index){
-		return elem.str();
+	return "(" + $.map(this.operands, function(evaled, index){
+		return evaled.str();
 	}).join(" OR ") + ")";
 };
 
 OR.prototype.eval = function(){
-	for (var i = 0; i < this.operands.length; i++) {
-		if(this.operands[i].eval()){
-			return true;
-		}
-	};
-	return false;
+	var evaled = $.map(this.operands, function(evaled, ind){
+		return evaled.eval() || evaled;
+	});
+	var literals = $.grep(evaled, isLiteral);
+	var symbols = $.grep(evaled, isNonLitereal);
+	// see the long comment in AND.prototype.eval
+	if(containsExpression(literals, TRUE))
+		return TRUE;
+	else if(symbols.length > 1)
+		return new OR(symbols);
+	else if(symbols.length == 1)
+		return symbols[0];
+	else
+		return FALSE;
 };
 
 /*********
@@ -69,35 +120,40 @@ OR.prototype.eval = function(){
  *********/
 
 function NOT(op){
-	this.operands = op;
+	this.operand = op;
 }
 
+NOT.prototype.TYPE = "OPERATOR";
+NOT.prototype.NAME = "NOT";
+
 NOT.prototype.str = function(){
-	return "(" + op.str() + ")'";
+	return "~(" + this.operand.str() + ")";
 };
 
 NOT.prototype.eval = function(){
-	return !op.eval();
+	var evaled = this.operand.eval();
+	if(evaled.TYPE == "LITERAL")
+		return evaled === TRUE ? FALSE : TRUE;
+	else
+		return new NOT(evaled);
 };
 
 /**************
  ** LITERALS **
  **************/
 
-var TRUE = {};
-TRUE.str = function(){
-	return "TRUE";
-};
-TRUE.eval = function(){
-	return true;
+var TRUE = {
+	TYPE: "LITERAL",
+	NAME: "TRUE",
+	str: function(){ return "TRUE"; },
+	eval: function() { return this; }
 };
 
-var FALSE = {};
-FALSE.str = function(){
-	return "FALSE";
-};
-FALSE.eval = function(){
-	return false;
+var FALSE = {
+	TYPE: "LITERAL",
+	NAME: "FALSE",
+	str: function(){ return "FALSE"; },
+	eval: function() { return this; }
 };
 
 /***************
@@ -108,12 +164,13 @@ function VARIABLE(name, expr){
 	this.name = name;
 	this.value = expr; // note: it is OK if this remains undefined!
 }
+VARIABLE.prototype.TYPE = "VARIABLE";
 VARIABLE.prototype.str = function(){
 	return this.name;
 };
 VARIABLE.prototype.eval = function(){
-	if(typeof this.expr === "undefined") return undefined;
-	else return this.expr.eval();
+	if(typeof this.value === "undefined") return this;
+	else return this.value.eval();
 };
 
 // TESTS
@@ -121,12 +178,13 @@ VARIABLE.prototype.eval = function(){
 	x = new VARIABLE("x");
 	y = new VARIABLE("y");
 	z = new AND([x, y]);
+	w = new NOT(z);
 
-	console.log(z.str());
+	console.log(w.str());
 	
-	console.log(z.eval());
+	console.log(w.eval().str());
 
 	x.value = TRUE;
-	y.value = TRUE;
-	console.log(z.eval());
+	//y.value = FALSE;
+	console.log(w.eval().str());
 })();
