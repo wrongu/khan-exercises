1da4fb2436d1ac84c90aa4bc927530173d25bac2
wrongu <lange.richard.d@gmail.com>
2013-10-09 15:36:59 -0400

    using 'arguments' parameter

--- utils/digital-logic.js
+++ utils/digital-logic.js
@@ -45,15 +45,15 @@ ExpressionError.prototype.name = "ExpressionError";
 /*********
  ** AND **
  *********/
-function AND(ops_array){
-	this.operands = ops_array;
+function AND(){
+	this.operands = arguments;
 }
 AND.prototype.TYPE = "OPERATOR";
 AND.prototype.NAME = "AND";
 
-AND.prototype.str = function(){
+AND.prototype.toString = function(){
 	return $.map(this.operands, function(evaled, index){
-		return evaled.str();
+		return evaled.toString();
 	}).join(" AND ");
 };
 
@@ -85,16 +85,16 @@ AND.prototype.eval = function(){
  ** OR  **
  *********/
 
-function OR(ops_array){
-	this.operands = ops_array;
+function OR(){
+	this.operands = arguments;
 }
 
 OR.prototype.TYPE = "OPERATOR";
 OR.prototype.NAME = "OR";
 
-OR.prototype.str = function(){
+OR.prototype.toString = function(){
 	return "(" + $.map(this.operands, function(evaled, index){
-		return evaled.str();
+		return evaled.toString();
 	}).join(" OR ") + ")";
 };
 
@@ -126,8 +126,8 @@ function NOT(op){
 NOT.prototype.TYPE = "OPERATOR";
 NOT.prototype.NAME = "NOT";
 
-NOT.prototype.str = function(){
-	return "~(" + this.operand.str() + ")";
+NOT.prototype.toString = function(){
+	return "~(" + this.operand.toString() + ")";
 };
 
 NOT.prototype.eval = function(){
@@ -145,14 +145,14 @@ NOT.prototype.eval = function(){
 var TRUE = {
 	TYPE: "LITERAL",
 	NAME: "TRUE",
-	str: function(){ return "TRUE"; },
+	toString: function(){ return "TRUE"; },
 	eval: function() { return this; }
 };
 
 var FALSE = {
 	TYPE: "LITERAL",
 	NAME: "FALSE",
-	str: function(){ return "FALSE"; },
+	toString: function(){ return "FALSE"; },
 	eval: function() { return this; }
 };
 
@@ -161,30 +161,204 @@ var FALSE = {
  ***************/
 
 function VARIABLE(name, expr){
-	this.name = name;
+	this.NAME = name;
 	this.value = expr; // note: it is OK if this remains undefined!
 }
 VARIABLE.prototype.TYPE = "VARIABLE";
-VARIABLE.prototype.str = function(){
-	return this.name;
+VARIABLE.prototype.toString = function(){
+	return this.NAME;
 };
 VARIABLE.prototype.eval = function(){
 	if(typeof this.value === "undefined") return this;
 	else return this.value.eval();
 };
 
+/*************
+ ** PARSING ** 
+ *************/
+
+function ParserContext(str){
+ 	this.str = str;
+ 	this.ind = 0;
+}
+ParserContext.prototype.set = function(i){
+	this.ind = i;
+};
+ParserContext.prototype.next = function(){
+	return this.str.charAt(this.ind++);
+};
+ParserContext.prototype.test = function(word){
+	if(this.str.substr(this.ind, word.length) === word){
+		this.ind += word.length;
+		return true;
+	} else{
+		return false;
+	}
+}
+ParserContext.prototype.match = function(word){
+	if(this.test(word)) return true;
+	else{
+		console.error("Parse Error: expected '"+word+"' at index "+this.ind);
+		return false;
+	}
+};
+ParserContext.prototype.isNum = function(){
+	var c = this.str.charAt(this.ind);
+	return '0' < c && c < '9';
+};
+ParserContext.prototype.isAlpha = function(){
+	var c = this.str.charAt(this.ind);
+	return ('a' < c && c < 'z') ||
+		('A' < c && c < 'Z') ||
+		c == '_';
+};
+// take whitespace
+ParserContext.prototype.s = function(){
+	while(this.test(" ") || this.test("\t") || this.test("\n"));
+};
+// get word (one or more alphanumeric or '_')
+ParserContext.prototype.w = function(){
+	var w = "";
+	while(this.isAlpha()) w += this.next();
+	return w;
+};
+
+var ExpressionParser = {
+
+	// THE TOP LEVEL
+	// parse full expression
+	expression: function(ctx){
+		// this line defines the order of operations
+		return this.parens(ctx) || 
+			this.not(ctx) || 
+			this.and(ctx) || 
+			this.or(ctx) || 
+			this.lit(ctx) || 
+			this.vari(ctx);
+	},
+
+	// anything not an operator ("unary", if you will)
+	term: function(ctx){
+		return this.parens(ctx) || 
+			this.lit(ctx) || 
+			this.vari(ctx);
+	},
+
+	// parse parenthesized expression and return it, or undefined if not there
+	parens: function(ctx){
+		ctx.s();
+		var start = ctx.ind;
+		if(ctx.test("(")){
+			var content = expression(ctx);
+			if(ctx.match(")")) return content;
+		}
+		// failed.. reset
+		ctx.set(start);
+		return undefined;
+	},
+
+	lit: function(ctx){
+		ctx.s();
+		if(ctx.test("TRUE") || ctx.test("T"))
+			return TRUE;
+		else if(ctx.test("FALSE") || ctx.test("F"))
+			return FALSE;
+		return undefined;
+	},
+
+	// parse a variable name
+	// variables are required to be a single capital letter. T and F are reserved.
+	vari: function(ctx){
+		ctx.s();
+		var start = ctx.ind;
+		var ch = ctx.next();
+		if('A' < ch && ch < 'Z' && ch != 'T' && ch != 'F')
+			return new VARIABLE(ch);
+		ctx.set(start);
+		return undefined;
+	},
+
+	not: function(ctx){
+		var start = ctx.ind;
+		ctx.s();
+		if(ctx.test("~") || ctx.test("!") || ctx.test("-")){
+			var content = this.term(ctx);
+			if(content) return new NOT(content);
+		}
+		// failed.. reset
+		ctx.set(start);
+		return undefined;
+	},
+
+	and: function(ctx){
+		var start = ctx.ind;
+		var lhs = this.term(ctx);
+		if(lhs){
+			ctx.s();
+			if(ctx.test("x") || ctx.test("AND") || ctx.test("A") || ctx.test("*")){
+				ctx.s();
+				var rhs = this.term(ctx) || this.not(ctx);
+				if(rhs) return new AND(lhs, rhs);
+			}
+		}
+		// failed.. reset
+		ctx.set(start);
+		return undefined;
+	},
+
+	or: function(ctx){
+		var start = ctx.ind;
+		var lhs = this.term(ctx);
+		if(lhs){
+			ctx.s();
+			if(ctx.test("+") || ctx.test("OR") || ctx.test("O")){
+				ctx.s();
+				var rhs = this.expression(ctx);
+				if(rhs) return new OR(lhs, rhs);
+			}
+		}
+		// failed.. reset
+		ctx.set(start);
+		return undefined;
+	},
+
+	// The function called externally
+	parse: function(str){
+		var ctx = new ParserContext(str);
+		return this.expression(ctx);
+	}
+};
+
 // TESTS
-(function(){
+(function(ctx){
 	x = new VARIABLE("x");
 	y = new VARIABLE("y");
-	z = new AND([x, y]);
+	z = new AND(x, y);
 	w = new NOT(z);
 
-	console.log(w.str());
+	console.log(w.toString());
 	
-	console.log(w.eval().str());
+	console.log(w.eval().toString());
 
 	x.value = TRUE;
 	//y.value = FALSE;
-	console.log(w.eval().str());
+	console.log(w.eval().toString());
 })();
+
+$(document).ready(function(){
+	$("#go_btn").eq(0).click(function(e){
+		console.log("PARSING: '"+$("#expr_in").eq(0).val()+"'");
+		var expr;
+		try{
+			expr = ExpressionParser.parse($("#expr_in").eq(0).val());
+		} catch(e){
+			$("#expr_out").eq(0).text(""+e);
+		}
+		if(expr){
+			$("#expr_out").eq(0).text(expr.toString());
+			$("#eval_out").eq(0).text(expr.eval().toString());
+		} else{
+			$("#expr_out").eq(0).text("errors of some sort");
+		}
+	});
+});
